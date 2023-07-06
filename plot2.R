fe645b40ee6fffac82672236430aefb0b741a9c4
Micha <129902707+RobMiChGu@users.noreply.github.com>
2023-07-07 00:04:48 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,5 +1,5 @@
 #Daten einlesen
-dataFile <- "d:/Data Science Foundations using R/4 Explorative Datenanalyse/Woche 1/household_power_consumption.txt"
+dataFile <- "household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 
 #nur Daten von 2007-02-01 und 2007-02-02
@@ -10,24 +10,7 @@ datetime <- strptime(paste(datenbereich$Date, datenbereich$Time, sep=" "), "%d/%
 
 globalActivePower <- as.numeric(datenbereich$Global_active_power)
 
-#Plot generieren mit 480x480 pixel
-#Achtung Wochentage auf deutsch Do, Fr, Sa
-png("d:/Data Science Foundations using R/4 Explorative Datenanalyse/Woche 1/plot2.png", width=480, height=480)
+#Plot generieren mit 480x480 pixel - Achtung Wochentage auf deutsch Do, Fr, Sa
+png("plot2.png", width=480, height=480)
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
